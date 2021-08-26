1ff91b9d01d8aaf4cfecd3e81efd2eecffae740f
Gregory Macat <gregorymacat@gmail.com>
2021-08-26 16:49:32 -0400

    Set up the directories

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [{
+        test: /\.(jsx|js)?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+    }]
+  },
+  mode: "development"
+}
