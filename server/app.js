fcf22267b06e29378266d69fad4e3b1e4a21547a
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-08-28 14:40:03 -0400

    changed port

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = 3001
 
-app.use('/', express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
