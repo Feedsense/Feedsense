8f79045c4e146b4989f21a3e54ee996c218e163d
Seth Baker <sethlbaker09@gmail.com>
2021-08-28 14:35:18 -0400

    Fixed the static server concern

--- server/app.js
+++ server/app.js
@@ -3,9 +3,7 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-// app.use('/', express.static(__dirname + '/../public'));
-app.use(express.static(__dirname + '/../public'));
-
+app.use('/', express.static(__dirname + '/../public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
