20aff5a61e1b5591b1cc34efdacc074bdb7ee875
Seth Baker <sethlbaker09@gmail.com>
2021-08-28 12:51:49 -0400

    Router setup with Analytics section

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.use('/', express.static(__dirname + '/../public'));
+// app.use('/', express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../public'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
