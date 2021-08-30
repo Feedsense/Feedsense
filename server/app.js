5292f353e01214ebd8f5679f9139d018f9aeef3a
Steven Klier <steven.klier@zenotech.io>
2021-08-30 14:02:59 -0500

    added hooks and stuff

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path');
-const app = express()
-const port = 3001
 
+const app = express();
+app.use(express.json());
 
 app.use(express.static(__dirname + '/../public'));
 
+
+const port = 3001;
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log('Success! Server Initialized');
 })
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Auth from './Auth.js';
 import config from '../../env/config.js';
 import GoogleLogin from 'react-google-login';
@@ -6,9 +6,24 @@ import GoogleLogin from 'react-google-login';
 
 var LandingPage = (props) => {
 
+  const [accessToken, setAccessToken] = useState('');
+  const [idToken, setIdToken] = useState('');
+  const [googleId, setGoogleId] = useState('');
+  const [email, setEmail] = useState('');
+  const [familyName, setFamilyName] = useState('');
+  const [givenName, setGivenName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
 
   const responseGoogle = (response) => {
     console.log(response);
+    setAccessToken(response.tokenObj.access_token);
+    setIdToken(response.tokenObj.id_token);
+    setGoogleId(response.googleId);
+    setEmail(response.profileObj.email);
+    setFamilyName(response.profileObj.email);
+    setGivenName(response.profileObj.givenName);
+    setImageUrl(response.profileObj.imageUrl);
 
     Auth.login(() => {
       props.history.push('/feed');
