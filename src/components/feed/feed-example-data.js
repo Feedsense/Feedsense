d24d1b7db5a36ec62a7e62beb8cc4f3f923d2e65
Griffin Georgiadis <griffingeorgiadis@gmail.com>
2021-08-30 17:06:01 -0400

    Framework feed (#8)
    * Fixing directories, including components folder
    
    * framework for feed page
    
    * fixing route to feed.jsx in app.jsx
    
    Co-authored-by: Gregory Macat <gregorymacat@gmail.com>

---
+++ src/components/feed/feed-example-data.js
@@ -0,0 +1,77 @@
+module.exports = [
+    {
+      'title': 'Mr.Beast Gives away 8 billion dollars',
+      'author': 'Mr.Beast',
+      'video' : 'https://www.youtube.com/watch?v=ktyJIj6i4Qw',
+      'body': 'Mr.Beast goes beast mode in giving away 8 billion dollars to bezos and   Obama',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'youtube'
+    },
+    {
+      'title': 'PewDiePie ',
+      'author': 'Mr.Beast',
+      'video': 'https://www.youtube.com/watch?v=Ky_OiCnKDqc',
+      'body': 'Mr.Beast goes beast mode in giving away 8 billion dollars to bezos and Obama',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'youtube'
+    },
+    {
+      'title': 'Mr.Beast Gives away 8 billion dollars',
+      'author': 'Mr.Beast'
+      'video': 'https://www.youtube.com/watch?v=ktyJIj6i4Qw',
+      'body': 'Mr.Beast goes beast mode in giving away 8 billion dollars to bezos and Obama',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'youtube'
+    },
+    {
+      'title': 'Rick and Morty - Get Schwifty',
+      'author': 'Mr.Beast'
+      'video': 'https://www.youtube.com/watch?v=4ctK1aoWuqY',
+      'body': 'Mr.Beast goes beast mode in giving away 8 billion dollars to bezos and Obama',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'youtube'
+    },
+    {
+      'title': 'Mr.Beast Gives away 8 billion dollars',
+      'author': 'Mr.Beast'
+      'video': 'https://www.youtube.com/watch?v=4ctK1aoWuqY',
+      'body': 'Mr.Beast goes beast mode in giving away 8 billion dollars to bezos and Obama',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'youtube'
+    },
+    {
+      'Author': 'Elon Musk'
+      'body': 'Do They even know about the Delta Varient?!?!',
+      'media': '',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'twitter'
+    },
+    {
+      'Author': 'Bill Gates',
+      'media': '',
+      'body': 'Avoiding a climate disaster will require a new, clean industrial revolution, and we don\'t have time to wait. Our work together will not only send us on a more viable path to net zero, but it will create long-term jobs in communities across the country.',
+      'createdAt':'2021-08-30T14:18:42-04:00',
+      'platform': 'twitter'
+    },
+    {
+      'Author': 'Barack Obama',
+      'body': 'May God bless the memory of those we lost, and protect those who remain in harm’s way.',
+      'media': '',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'twitter'
+    },
+    {
+      'Author': 'Jack Black',
+      'body': 'KungFu Panda 3 out Noq!',
+      'media': '',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'twitter'
+    },
+    {
+      'Author': 'Dwanye Johnson',
+      'body': 'I am the rock',
+      'media': '',
+      'createdAt': '2021-08-30T14:18:42-04:00',
+      'platform': 'twitter'
+    },
+];
