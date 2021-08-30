1e47a7fe4ac0b370b5194210a5fb5cd7263aa38f
Steven Klier <steven.klier@zenotech.io>
2021-08-30 11:22:25 -0500

    slight changes to landing page google login

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import Auth from './Auth.js'
+import { GoogleLogin } from 'react-google-login';
 
 var LandingPage = (props) => {
 
-  var login = () => {
+  const responseGoogle = (res) => {
+    console.log(res)
     Auth.login(() => {
       props.history.push('/feed');
     })
   }
-
+  
   return (
     <div>
       Hello World
       <button onClick={login}>Login</button>
+      <GoogleLogin
+        clientId="600013893616-bdhqvs5cbrnmjoucotaj1jt2tg5k4vbh.apps.googleusercontent.com"
+        buttonText="Login"
+        redirectUri='http://localhost:3000/feed'
+        onSuccess={login}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   )
 };
