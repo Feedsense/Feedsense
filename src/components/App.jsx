e7f50f030b3600a95e945122908532b517a0d720
Steven Klier <steven.klier@zenotech.io>
2021-08-30 19:36:20 -0500

    sign in is 90% there

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {HashRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
 
 import ProtectedRoute from './ProtectedRoute.js'
 import LandingPage from './LandingPage.jsx';
@@ -8,12 +8,23 @@ import Analytics from './Analytics/Analytics.jsx';
 
 
 var App = () => {
+
+  const [isGoogleSignedIn, setIsGoogleSignedIn] = useState(false)
+
   return (
     <div className='mainContainer'>
       <Router>
         <Switch>
-          <Route exact path="/" component={LandingPage}/>
-          <ProtectedRoute exact path="/feed" component={Feed}/>
+          <Route exact path="/">
+            <LandingPage 
+              setIsGoogleSignedIn={setIsGoogleSignedIn}
+              isGoogleSignedIn={isGoogleSignedIn}/>
+          </Route>
+          <ProtectedRoute exact path="/feed">
+            <Feed 
+            setIsGoogleSignedIn={setIsGoogleSignedIn}
+            isGoogleSignedIn={isGoogleSignedIn}/>
+          </ProtectedRoute>
           <ProtectedRoute exact path="/Analytics/Analytics" component={Analytics}/>
           <Route to='*' component={() => ('404 not found')}/>
         </Switch>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Auth from './Auth.js';
 import config from '../../env/config.js';
-import GoogleLogin from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 
 
-var LandingPage = (props) => {
+var LandingPage = ({ isGoogleSignedIn, setIsGoogleSignedIn }) => {
+
+  const history = useHistory();
 
   const [accessToken, setAccessToken] = useState('');
   const [idToken, setIdToken] = useState('');
@@ -16,18 +19,34 @@ var LandingPage = (props) => {
 
 
   const responseGoogle = (response) => {
-    console.log(response);
+
+    console.log(Auth.isAuthenticated());
+
+
+    console.log(history);
+
     setAccessToken(response.tokenObj.access_token);
     setIdToken(response.tokenObj.id_token);
     setGoogleId(response.googleId);
     setEmail(response.profileObj.email);
-    setFamilyName(response.profileObj.email);
+    setFamilyName(response.profileObj.familyName);
     setGivenName(response.profileObj.givenName);
     setImageUrl(response.profileObj.imageUrl);
 
+    localStorage.clear();
+    localStorage.access_token = response.tokenObj.access_token;
+    localStorage.id_token = response.tokenObj.id_token;
+    localStorage.google_id = response.googleId;
+    localStorage.email = response.profileObj.email;
+    localStorage.family_name = response.profileObj.familyName;
+    localStorage.given_name = response.profileObj.givenName;
+    localStorage.image_url = response.profileObj.imageUrl;
+
+    
     Auth.login(() => {
-      props.history.push('/feed');
+      history.push('/feed');
     })
+    
   }
 
   return (
@@ -40,6 +59,8 @@ var LandingPage = (props) => {
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+        // redirectUri={Auth.login(() => history.push('/feed'))}
       />
     </div>
   )
