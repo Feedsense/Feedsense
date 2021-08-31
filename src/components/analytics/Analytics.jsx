2c2697e5dcd5834721779802c6d0fb9528a0ffac
Seth Baker <sethlbaker09@gmail.com>
2021-08-31 16:14:59 -0400

    Graph functionality complete

--- src/components/analytics/Analytics.jsx
+++ src/components/analytics/Analytics.jsx
@@ -14,10 +14,12 @@ const Analytics = (props) => {
   }
 
   const [fakeTwitterData, SetFakeTwitterData] = useState({followers: 35910,
-    subscriberGrowth: [14032, 15284, 19284, 19894, 20183, 22013, 26019, 27194, 27519, 29475, 34913, 35910]
+    subscriberGrowth: [14032, 15284, 19284, 19894, 20183, 22013, 26019, 27194, 27519, 29475, 34913, 35910],
+    monthlyComments: [1943, 1039, 1834, 1092, 2894, 4928, 5993, 5193, 5419, 5219, 7019, 7192]
   })
   const [fakeYoutubeData, SetFakeYoutubeData] = useState({subscribers: 192412,
-    subscriberGrowth: [24119, 25001, 26991, 27102, 87029, 98192, 109205, 127952, 146295, 169300, 183183, 192412]
+    subscriberGrowth: [24119, 25001, 26991, 27102, 87029, 98192, 109205, 127952, 146295, 169300, 183183, 192412],
+    monthlyComments: [8135, 8175, 9105, 10284, 10829, 12890, 19825, 25981, 17280, 19283, 18395, 24993]
   })
 
 
--- src/components/analytics/Dashboard/GrowthGraph.jsx
+++ src/components/analytics/Dashboard/GrowthGraph.jsx
@@ -1,21 +1,28 @@
-import React, {useContext} from 'react';
+import React, { useState, useContext, Component } from 'react';
+import { TwitterContext, YouTubeContext } from '../Analytics.jsx';
 import Chart from 'react-apexcharts';
-import {TwitterContext, YouTubeContext} from '../Analytics.jsx';
+import Switch from 'react-switch';
+
+
 
 const GrowthGraph = () => {
-
   const twitterData = useContext(TwitterContext);
   const youtubeData = useContext(YouTubeContext);
+
+  const [checked, setChecked] = useState(false)
+  const [twitterGraphData, setTwitterGraphData] = useState(twitterData.subscriberGrowth)
+  const [youtubeGraphData, setYoutubeGraphData] = useState(youtubeData.subscriberGrowth)
+  const [graphTitle, setGraphTitle] = useState('Subscriber Growth')
 
   var growthChart = {
     series: [
       {
         name: 'Twitter',
-        data: twitterData.subscriberGrowth
+        data: twitterGraphData
       },
       {
         name: 'YouTube',
-        data: youtubeData.subscriberGrowth
+        data: youtubeGraphData
       }
     ],
     chartOptions: {
@@ -23,20 +30,52 @@ const GrowthGraph = () => {
       stroke: {
         curve: 'smooth'
       },
+      dataLabels: {
+        enabled: false
+      },
       colors: ['#2E93fA', '#63FFC1'],
-      // xasis: {
-      //   categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      // }
+      xasis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      }
+    }
+  }
+
+  const flipSwitch = (nextChecked) => {
+    setChecked(nextChecked)
+
+    if (checked === true) {
+      setGraphTitle('Subscriber Growth')
+      setTwitterGraphData(twitterData.subscriberGrowth)
+      setYoutubeGraphData(youtubeData.subscriberGrowth)
+    } else {
+      setGraphTitle('Comments per Month')
+      setTwitterGraphData(twitterData.monthlyComments)
+      setYoutubeGraphData(youtubeData.monthlyComments)
     }
   }
 
   return (
-    <Chart
-    options={growthChart.chartOptions}
-    series={growthChart.series}
-    type='line'
-    width='700'
-    />
+    <div>
+      <h3>{graphTitle}</h3>
+      <div>
+        <h4>Subscribers</h4>
+        <Switch
+          onChange={flipSwitch}
+          checked={checked}
+          onColor="#86d3ff"
+          offColor="#86d3ff"
+          uncheckedIcon={false}
+          checkedIcon={false}
+          />
+        <h4>Comments</h4>
+      </div>
+      <Chart
+        options={growthChart.chartOptions}
+        series={growthChart.series}
+        type='area'
+        width='700'
+      />
+    </div>
   )
 }
 
