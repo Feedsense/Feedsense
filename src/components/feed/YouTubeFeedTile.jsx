92978191bc031bada52a4837083208b99375e322
Griffin Georgiadis <griffingeorgiadis@gmail.com>
2021-09-01 10:09:25 -0400

    inital feed set up

--- src/components/feed/YouTubeFeedTile.jsx
+++ src/components/feed/YouTubeFeedTile.jsx
@@ -7,8 +7,8 @@ var YouTubeFeedTile = (props) => {
     <div className="post-tile">
       <i class="YT-icon fab fa-youtube"></i>
       <div className="timestamp">{moment(props.postData.createdAt).startOf('hour').fromNow()}</div>
-      <div className="title"><b>Title:</b>{props.postData.title}</div>
-      <div className="youtube-author"><b>Author:</b>{props.postData.author}</div>
+      <div className="title"><b className="prefix">Title:</b>{props.postData.title}</div>
+      <div className="youtube-author"><b className="prefix">Channel:</b>{props.postData.author}</div>
       <br></br>
       <div className="youtube-body">{props.postData.body}</div>
       <div className="youtube-media">{props.postData.video}</div>
