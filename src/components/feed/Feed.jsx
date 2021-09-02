07ee82f5234ab0087ad326e6ce48a1c1ee53a2d6
Griffin Georgiadis <griffingeorgiadis@gmail.com>
2021-09-01 20:42:01 -0400

    sorted youtube data

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,102 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import config from '../../../env/config.js';
-import Auth from '../Auth.js';
-import {Switch, Link} from 'react-router-dom';
-import feedExampleData from './feed-example-data.js';
-import TwitterFeedTile from './TwitterFeedTile.jsx';
-import YouTubeFeedTile from './YouTubeFeedTile.jsx';
-import '../../feedStyle.css';
-import { useHistory } from 'react-router-dom';
-import { useGoogleLogin } from 'react-google-login';
-import '../../style.css';
-import Modal from '../modals/Modal.jsx';
-import ModalViewer from '../modals/useModal.jsx'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import config from "../../../env/config.js";
+import Auth from "../Auth.js";
+import { Switch, Link } from "react-router-dom";
+import feedExampleData from "./feed-example-data.js";
+import TwitterFeedTile from "./TwitterFeedTile.jsx";
+import YouTubeFeedTile from "./YouTubeFeedTile.jsx";
+import "../../feedStyle.css";
+import { useHistory } from "react-router-dom";
+import { useGoogleLogin } from "react-google-login";
+import "../../style.css";
+import Modal from "../modals/Modal.jsx";
+import ModalViewer from "../modals/useModal.jsx";
+import axios from "axios";
 
 var Feed = ({ setIsGoogleSignedIn }) => {
+  const { isShowing, toggle } = ModalViewer();
 
-  const {isShowing, toggle} = ModalViewer();
-
-  const [ exampleData, setExampleData ] = useState(feedExampleData);
-  const [ socialMediaData, setsSocialMediaData ] = useState([]);
+  const [exampleData, setExampleData] = useState(feedExampleData);
+  const [socialMediaData, setSocialMediaData] = useState([]);
 
   const history = useHistory();
 
-  const {signIn} = useGoogleLogin({
+  const { signIn } = useGoogleLogin({
     onSuccess: (res) => {
-      console.log(res)
+      console.log(res);
     },
     clientId: config.clientId,
     isSignedIn: true,
     onFailure: (err) => console.log(err),
-  })
+  });
 
   var logout = () => {
     Auth.logout(() => {
-      history.push('/');
-    })
-  }
+      history.push("/");
+    });
+  };
 
   useEffect(() => {
-    signIn()
+    signIn();
     setIsGoogleSignedIn(true);
 
-    if( localStorage.access_token) {
-      axios.get(`/getYoutube/${localStorage.access_token}`)
-        .then(data => {
-          setSocialMediaData(data);
+    if (localStorage.access_token) {
+      axios
+        .get(`/getYoutube/${localStorage.access_token}`)
+        .then((data) => {
+          setSocialMediaData(data.data);
         })
-        .catch( err => {
-          console.error('ERROR RETRIEVING DATA: ', err.stack);
+        .catch((err) => {
+          console.error("ERROR RETRIEVING DATA: ", err.stack);
         });
     }
-  }, [])
+  }, []);
+
+  var sortedSocialmedia = [];
 
 
-
+  if (socialMediaData.length > 0) {
+    for (var i = 0; i < socialMediaData.length; i++) {
+      if (sortedSocialmedia.length === 0) {
+        sortedSocialmedia.push(socialMediaData[i]);
+      } else {
+        for (var j = 0; j < sortedSocialmedia.length; j++) {
+          if (socialMediaData[i].snippet.publishedAt > sortedSocialmedia[j].snippet.publishedAt) {
+            sortedSocialmedia.splice(j, 0,  socialMediaData[i]);
+            break;
+          }
+          if (j === sortedSocialmedia.length - 1) {
+            sortedSocialmedia.push(socialMediaData[i]);
+            break;
+          }
+        }
+      }
+    }
+  }
+  console.log(sortedSocialmedia);
 
   return (
     <div>
-
-      <div className='header'>
+      <div className="header">
         <div>
-          <h1 >Feedsense</h1>
+          <h1>Feedsense</h1>
         </div>
-        <div className='navButtonContainer'>
-          <Link to='/Analytics/Analytics'>Analytics</Link>
+        <div className="navButtonContainer">
+          <Link to="/Analytics/Analytics">Analytics</Link>
           <button onClick={toggle}>Post to Social Media</button>
-          <Modal
-            isShowing={isShowing}
-            hide={toggle}
-          />
-          <a className='logout-btn' onClick={() => {
-            setIsGoogleSignedIn(false);
-            const auth2 = window.gapi.auth2.getAuthInstance()
-            if (auth2 != null) {
-              auth2.signOut().then(
-                auth2.disconnect()
-              )
-            }
-            localStorage.clear();
-            logout();
-          }
-          }>logout</a>
+          <Modal isShowing={isShowing} hide={toggle} />
+          <a
+            className="logout-btn"
+            onClick={() => {
+              setIsGoogleSignedIn(false);
+              const auth2 = window.gapi.auth2.getAuthInstance();
+              if (auth2 != null) {
+                auth2.signOut().then(auth2.disconnect());
+              }
+              localStorage.clear();
+              logout();
+            }}
+          >
+            logout
+          </a>
         </div>
       </div>
 
-
       <div>
-        {exampleData.map((post, index) => {
-          if (post['platform'] === 'youtube') {
-            return <YouTubeFeedTile key={index} postData={post}/>
-          }
-          if (post['platform'] === 'twitter') {
-            return <TwitterFeedTile key={index} postData={post}/>
-          }
+        {sortedSocialmedia.map((post, index) => {
+          // if (post.kind.includes('youtube')) {
+          return <YouTubeFeedTile key={index} postData={post.snippet} />;
+          // }
+          // if (post['platform'] === 'twitter') {
+          //   return <TwitterFeedTile key={index} postData={post}/>
+          // }
         })}
       </div>
     </div>
   );
-}
+};
 
 export default Feed;
--- src/components/feed/YouTubeFeedTile.jsx
+++ src/components/feed/YouTubeFeedTile.jsx
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import moment from "moment";
 
-var YouTubeFeedTile = (props) => (
-  <div className="YT-post-tile">
-    <div className="tile-img">
-      <img className="YT-img" src={props.postData.image}></img>
+
+var YouTubeFeedTile = (props) => {
+  var desc = props.postData.description;
+  if(desc.length > 250) {
+    desc = desc.substring(0, 250) + '...';
+  }
+  return (
+    <div className="YT-post-tile">
+      <div className="tile-img">
+        <img className="YT-img" src={props.postData.thumbnails.default.url}></img>
+      </div>
+      <div className="tile-content">
+        <i className="YT-icon fab fa-youtube"></i>
+        <div className="timestamp">
+          {moment(props.postData.publishedAt).startOf("hour").fromNow()}
+        </div>
+        <div className="title">
+          <b className="prefix">Title:</b>
+          {props.postData.title}
+        </div>
+        <div className="youtube-author">
+          <b className="prefix">Channel:</b>
+          {props.postData.channelTitle}
+        </div>
+        <br></br>
+        <div className="youtube-body">{desc}</div>
+      </div>
     </div>
-    <div className="tile-content">
-      <i className="YT-icon fab fa-youtube"></i>
-      <div className="timestamp">
-        {moment(props.postData.createdAt).startOf("hour").fromNow()}
-      </div>
-      <div className="title">
-        <b className="prefix">Title:</b>
-        {props.postData.title}
-      </div>
-      <div className="youtube-author">
-        <b className="prefix">Channel:</b>
-        {props.postData.author}
-      </div>
-      <br></br>
-      <div className="youtube-body">{props.postData.body}</div>
-    </div>
-  </div>
-);
+    );
+};
 
 export default YouTubeFeedTile;
