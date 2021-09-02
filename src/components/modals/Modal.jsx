8486a8a37409abaac4fae56b140d6d0500ce215e
Gregory Macat <gregorymacat@gmail.com>
2021-09-02 09:49:16 -0400

    Adding form

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const config = {
+  headers: {
+    'Content-Type': 'multipart/form-data'
+  },
+  onUploadProgress: (progressEvent) => {
+    var percentCompleted = Math.round(
+      (progressEvent.loaded * 100) / progressEvent.total
+    );
+    console.log(percentCompleted);
+  }
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const userData = new FormData(event.target);
+  console.log(userData);
+}
+
 const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
     <div className="modal-overlay"/>
@@ -11,9 +30,18 @@ const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <p>
-          Hello, I'm a modal.
-        </p>
+        <form>
+          <h2>Twitter</h2>
+          <h2>Youtube</h2>
+          <label>Title</label>
+          <input id="title" type="text" placeholder="Example Title"></input>
+          <label>Description</label>
+          <input id="description" type="text" placeholder="Write your description of the video here..."></input>
+          <label>Tags</label>
+          <input id="tags" type="text" placeholder="Separate your tags by comma here..."></input>
+          <label>Upload Video</label>
+          <input id="video" type="file"></input>
+        </form>
       </div>
     </div>
   </React.Fragment>, document.body
